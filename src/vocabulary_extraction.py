5bdf1897960cb166150010681172db4b2cf6d1f8
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2025-02-21 12:29:56 +0100

    feat: remove unused methods and add search for curi and puri

--- src/vocabulary_extraction.py
+++ src/vocabulary_extraction.py
@@ -1,81 +1,17 @@
 import json
+import re
 import time
 
 import pandas as pd
-import rdflib.util
 import requests
-from SPARQLWrapper import SPARQLWrapper
-from rdflib import Graph
 
 from src.preprocessing import merge_dataset
-from src.util import is_endpoint_working
 
-return_formats = ['JSON', 'XML', 'TURTLE', 'N3', 'RDF', 'RDFXML', 'CSV', 'TSV', 'JSONLD']
-
-
-def load_endpoint_list():
-    return pd.read_csv("../data/raw/sparql_full_download.csv")['sparql_url']
+CLEAN_URI = re.compile(r'^([^#]+)?')
 
 
-def find_vocabulary_tag(endpoint, limit=5):
-    query_string = """
-   SELECT ?vocab
-WHERE {
-  ?s ?p ?vocab . # Bind ?vocab to the object of the triple
-  FILTER(isIRI(?vocab)) # Ensure it's an IRI (vocabulary term)
-} LIMIT 10"""
-    sparql = SPARQLWrapper(endpoint)
-    sparql.setQuery(query_string)
-    sparql.setTimeout(120)
-    try:
-        sparql.setReturnFormat("json")
-        results = sparql.query().convert()
-        return _get_lov_tag(results)
-    except Exception as e:
-        return None
-
-
-def find_vocabulary_local(path: str):
-    g = Graph()
-    result = g.parse(path, format=rdflib.util.guess_format(path))
-    qres = result.query("""
-   SELECT DISTINCT ?vocab
-WHERE {
-  ?s ?p ?vocab . 
-  FILTER(isIRI(?vocab)) # Ensure it's an IRI (vocabulary term)
-  FILTER(!CONTAINS(STR(?vocab), "w3.org")) # Exclude results from w3.org
-}""")
-    for row in qres:
-        print(row.vocab)
-        print(requests.get(f"https://lov.linkeddata.es/dataset/lov/api/v2/vocabulary/info?vocab={row.vocab}").text)
-        print(requests.get(f"https://coli-conc.gbv.de/api/concepts?uri={row.vocab}").text)
-
-
-def _get_lov_tag(sparql_res):
-    tags = []
-    for result in sparql_res['results']['bindings']:
-        res = result['vocab']['value']
-        status = requests.get(f"https://lov.linkeddata.es/dataset/lov/api/v2/vocabulary/info?vocab={res}")
-        if status.status_code != 404:
-            text = json.loads(status.text)
-            tags.append(text['tags'])
-            print(text['tags'])
-        else:
-            status = requests.get(f"https://coli-conc.gbv.de/api/concepts?uri={res}")
-            if status.status_code != 404:
-                print(status.text)
-        return tags
-
-
-def search_from_list():
-    for endpoint in load_endpoint_list():
-        if is_endpoint_working(endpoint):
-            find_vocabulary_tag(endpoint)
-
-
-def find_tags_from_json():
-    df = merge_dataset()
-    response_df = pd.DataFrame(columns=['id', 'tags', 'category'])
+def find_tags_from_json(df: pd.DataFrame):
+    response_df = pd.DataFrame(columns=['id', 'tags', 'voc','category'])
     subject_list = []
     response_cache = {}
     for index, row in df.iterrows():
@@ -88,7 +24,7 @@ def find_tags_from_json():
                 time.sleep(0.1)
             else:
                 response_lov = response_cache[voc]
-            if response_lov.status_code != 404:
+            if response_lov.status_code == 200:
                 try:
                     response_dict = json.loads(response_lov.text)
                     tags = response_dict['tags']
@@ -100,12 +36,74 @@ def find_tags_from_json():
                     response_df.loc[len(response_df)] = {
                         'id': row['id'],
                         'tags': frame_tags,
+                        'voc': voc,
                         'category': row['category']
                     }
-
-                    response_df.to_json('../data/raw/vocabulary_tag.json', index=False)
                 except Exception as e:
                     print(e)
-            print(requests.get(f"https://coli-conc.gbv.de/api/concepts?uri={voc}").text)
+
 
     return subject_list
+
+def find_tags_from_json_curi_puri(df: pd.DataFrame):
+    response_curi_puri = pd.DataFrame(columns=['id', 'curi', 'puri' 'tags_curi', 'tags_puri', 'tags_voc', 'category'])
+
+    curi_dict = {}
+    puri_dict = {}
+
+    for index, row in df.iterrows():
+        list_curi_tags = set()
+        list_puri_tags = set()
+
+        for curi in row['curi']:
+            response_lov = _get_lov_search_result(curi, curi_dict)
+            if response_lov != '':
+                list_curi_tags.add(response_lov)
+            else:
+                list_curi_tags.add(None)
+
+        for puri in row['puri']:
+            response_lov = _get_lov_search_result(puri, puri_dict)
+            if response_lov != '':
+                list_puri_tags.add(response_lov)
+            else:
+                list_puri_tags.add(None)
+
+        response_curi_puri.loc[len(response_curi_puri)] = {
+            'id': row['id'],
+            'curi': row['curi'],
+            'puri': row['puri'],
+            'tags_curi': list_curi_tags,
+            'tags_puri': list_puri_tags,
+            'category': row['category']
+        }
+
+    response_curi_puri.to_json('../data/raw/curi_puri_tags.json', orient='records', index=False)
+
+def _get_lov_search_result(uri, cache_dict) -> frozenset | str:
+    clean_uri = CLEAN_URI.search(uri)
+    clean_uri = clean_uri.group(1)
+    if clean_uri not in cache_dict:
+        response_lov = requests.get(f"https://lov.linkeddata.es/dataset/lov/api/v2/term/search?q={uri}",
+                                 timeout=300)
+        if response_lov.status_code == 200:
+            key_set = set()
+            for keys in json.loads(response_lov.text)['aggregations']['tags']['buckets']:
+                key_set.add((keys['key'], keys['doc_count']))
+            if len(key_set) > 0:
+                print(clean_uri)
+                frozenset_set = frozenset(key_set)
+                cache_dict[clean_uri] = frozenset_set
+                response_lov = frozenset_set
+                print(frozenset_set)
+                return response_lov
+        cache_dict[clean_uri] = ''
+        time.sleep(0.1)
+        response_lov = ''
+    else:
+        response_lov = cache_dict[clean_uri]
+
+    return response_lov
+
+df = merge_dataset()
+find_tags_from_json_curi_puri(df)
