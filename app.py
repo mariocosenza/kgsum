d162d78cb8a86ebaee70822ac83782766fff1452
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2025-02-24 16:42:33 +0100

    feat: add basic route and flasgger library to use swagger ui

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+from flasgger import Swagger
 from flask import Flask
-
 app = Flask(__name__)
+swagger = Swagger(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
+@app.route('/api/v1/sparql/category')
+def sparql_category():
+    return ''
+
+@app.route('/api/v1/dump/category')
+def rdf_category():
+    return ''
+
+@app.route('/api/v1/search')
+def search():
+    return ''
+
+@app.route('/api/v1/info')
+def info_from_db():
+    return ''
 
 if __name__ == '__main__':
     app.run()
