2d6586269336f795d7c2b16e3d306c2703d1e37b
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-03-01 12:21:54 +0100

    fix: make remove empty rows function more flexible

--- src/dataset_cleanup.py
+++ src/dataset_cleanup.py
@@ -1,27 +1,26 @@
 import pandas as pd
 
-def remove_empty_rows(frame: pd.DataFrame, label) -> pd.DataFrame:
-    match label:
-        case 'lab':
-            return frame[frame.lab != '']
-        case 'lcn':
-            return frame[frame.lcn != '']
-        case 'lpn':
-            return frame[frame.lnp != '']
-        case 'sbj':
-            return frame[frame.sbj != '']
-        case 'dsc':
-            return frame[frame.dsc != '']
-        case 'curi':
-            return frame[frame.curi != '']
-        case 'puri':
-            return frame[frame.puri != '']
-        case 'voc':
-            return frame[frame.voc != '']
-        case 'tdl':
-            return frame[frame.tdl != '']
-        case _:
-            return frame
 
+def remove_empty_rows(frame: pd.DataFrame, labels: list | str) -> pd.DataFrame:
+    if isinstance(labels, str):
+        labels = [labels]
 
+    result = frame.copy()
+    column_map = {
+        'lab': 'lab',
+        'lcn': 'lcn',
+        'lnp': 'lnp',
+        'sbj': 'sbj',
+        'dsc': 'dsc',
+        'curi': 'curi',
+        'puri': 'puri',
+        'voc': 'voc',
+        'tdl': 'tdl'
+    }
 
+    for label in labels:
+        if label in column_map:
+            column = column_map[label]
+            result = result[result[column] != '']
+
+    return result
