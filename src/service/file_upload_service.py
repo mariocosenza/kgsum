2005435baa8089117191a603f478a85e1cfb676e
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-07-19 09:08:57 +0200

    fix: add os.makedirs

--- src/service/file_upload_service.py
+++ src/service/file_upload_service.py
@@ -3,6 +3,7 @@ import os
 ALLOWED_EXTENSIONS = {'xml', 'trig', 'ttl', 'nq', 'nt', 'rdf'}
 UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', os.path.abspath('./uploads'))
 
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 def allowed_file(filename):
     return '.' in filename and \
